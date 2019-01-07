c32e99659359694df8f65b9f74e71cce5c8c4964
EbaiNsoatabe <ebainsoatabe@googlemail.com>
2019-01-07 17:54:11 +0000

    init commit

---
+++ src/main/scala/UniqueAddition.scala
@@ -0,0 +1,28 @@
+object UniqueAddition extends App{
+  var sum = 0
+
+  def add(a:Int, b:Int, c:Int):Int={
+    if(a != b && a != c && b != c){
+      sum = a + b + c
+    }
+    else if(a == b && a != c){
+      sum = c
+    }
+    else if(b == c && a != b){
+      sum = a
+    }
+    else if(a == c && b != a){
+      sum = b
+    }
+    return sum
+  }
+
+  print("Enter number: ")
+  val firstNum = scala.io.StdIn.readInt()
+  print("Enter number: ")
+  var secondNum = scala.io.StdIn.readInt()
+  print("Enter number: ")
+  var thirdNum = scala.io.StdIn.readInt()
+
+  print(add(firstNum, secondNum, thirdNum))
+}
