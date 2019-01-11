0eaeb0a1759dcedcac75462704ccbf039ecf8d18
EbaiNsoatabe <ebainsoatabe@googlemail.com>
2019-01-11 15:41:40 +0000

    Merge branch 'master' of https://github.com/EbaiNsoatabe/ScalaTasks into wednesday
    # Conflicts:
    #	src/main/scala/LetterSquare.scala
    #	src/main/scala/fizzbuzz.scala

---
+++ src/main/scala/ZerosCount.scala
@@ -0,0 +1,18 @@
+object ZerosCount extends App{
+  val numGroup = Array(0, 6, 6, 7, 7, 8, 8, 8, 0, 0, 43)
+  var count = 0
+  var index = 0
+
+  def zeroChecker(num:Int): Unit ={
+    if(num == 0){
+      count += 1
+    }
+    index += 1
+    if(index < numGroup.length) {
+      zeroChecker(numGroup(index))
+    }
+  }
+
+  zeroChecker(numGroup(index))
+  println(s"There are $count zeros in the list")
+}
